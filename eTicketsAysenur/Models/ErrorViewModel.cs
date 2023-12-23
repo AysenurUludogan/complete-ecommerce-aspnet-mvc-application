a6103e5fe98201d850f546b0019ac0f3adb1f703
Aysenur <aysenuruludogan@gmail.com>
2023-12-23 14:36:56 +0300

    Proje dosyası ekle.

---
+++ eTicketsAysenur/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicketsAysenur.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
